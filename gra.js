eda1b714c0b5013da74bea3e79c04b7403cfcd31
mateki0 <mateki0@interia.pl>
2020-04-22 10:39:05 +0200

    Get random phrase completed

--- gra.js
+++ gra.js
@@ -1,4 +1,5 @@
 let categories = [];
+let allCat = []
 
 function getQuery() {
   let query = window.location.search.substring(1)
@@ -7,63 +8,71 @@ function getQuery() {
     categories.push(vars[i].slice(0, vars[i].indexOf('=')))
   }
 }
+const allCategories = {
+  cities: [
+    'Wagadugu',
+    'Jamasukro',
+    'Pretoria',
+    'Windhuk',
+    'Dżibuti',
+    'Rangun',
+    'Caracas',
+    'Ndżamena',
+    'Niamej',
+    'Bissau',
+  ],
+  sport: [
+    'tłoki gorzyce',
+    'błękitni roposzyce',
+    'kmita zabierzów',
+    'górnik wałbrzych',
+    'rozwój katowice',
+    'ruch radzionków',
+    'aluminium konin',
+    'podbeskidzie bielsko biała',
+  ],
+  countries: [
+    'Islamska Republika Afganistanu',
+    'Księstwo Andory',
+    'Republika Beninu',
+    'Państwo Brunei Darussalam',
+    'Republika Dżibuti',
+    'Republika Gwinei',
+    'Jordańskie Królestwo Haszymidzkie',
+    'Sułtanat Omanu',
+    'Republika Seszeli',
+    'Republika Sierra Leone',
+  ],
+  people: [
+    'Alexander Graham Bell',
+    'Albert Einstein',
+    'Pitagoras',
+    'Bill Gates',
+    'Walt Disney',
+    'Thomas Edison',
+    'Chuck Norris',
+  ]
+}
+
+function getCategory() {
+  let keys = Object.keys(allCategories)
+  categories.map(cat => {
+    keys.map(key => {
+      let value = allCategories[key]
+      if (cat == key) {
+        allCat.push(value)
+      }
+    })
+  })
+  return allCat
+}
+
+function getOneRandomCategory() {
+  let min = 0;
+  let max = allCat.flat().length;
+  let random = Math.floor(Math.random() * (max - min)) + min;
+  return allCat.flat()[random]
+}
 getQuery()
-console.log(categories)
-
-
-
-
-
-
-
-
-
-// Categories
-
-var miasta = [
-  'Wagadugu',
-  'Jamasukro',
-  'Pretoria',
-  'Windhuk',
-  'Dżibuti',
-  'Rangun',
-  'Caracas',
-  'Ndżamena',
-  'Niamej',
-  'Bissau',
-];
-
-
-var sport = [
-  'tłoki gorzyce',
-  'błękitni roposzyce',
-  'kmita zabierzów',
-  'górnik wałbrzych',
-  'rozwój katowice',
-  'ruch radzionków',
-  'aluminium konin',
-  'podbeskidzie bielsko biała',
-];
-
-var panstwa = [
-  'Islamska Republika Afganistanu',
-  'Księstwo Andory',
-  'Republika Beninu',
-  'Państwo Brunei Darussalam',
-  'Republika Dżibuti',
-  'Republika Gwinei',
-  'Jordańskie Królestwo Haszymidzkie',
-  'Sułtanat Omanu',
-  'Republika Seszeli',
-  'Republika Sierra Leone',
-]
-
-var osoby = [
-  'Alexander Graham Bell',
-  'Albert Einstein',
-  'Pitagoras',
-  'Bill Gates',
-  'Walt Disney',
-  'Thomas Edison',
-  'Chuck Norris',
-]
+getCategory()
+getOneRandomCategory()
